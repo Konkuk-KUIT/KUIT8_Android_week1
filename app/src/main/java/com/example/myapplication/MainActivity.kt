766ef06a4ce4aac2a2bf68ab8c44fa569f361bbe
일혁 <dnfjddk2@gmail.com>
2026-09-06 07:20:14 +0900

    chore: 1주차에는 preview-public-check 를 끈다
    라이브 템플릿 prev 가 만드는 public @Preview 가 강의 자료와 같아야 한다.
    가시성 수식어를 다루는 주차에 이 줄을 지운다.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -43,7 +43,7 @@ fun Greeting(
 
 @Preview(showBackground = true)
 @Composable
-private fun GreetingPreview() {
+fun GreetingPreview() {
     MyApplicationTheme {
         Greeting("Android")
     }
